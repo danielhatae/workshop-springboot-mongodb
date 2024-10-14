b4c18feba119bdec857a6faad5b8c0c9066d9199
danielhatae <daniel.hatae@gmail.com>
2024-10-14 14:23:05 -0300

    Obtendo um post por id

---
+++ src/main/java/com/daniel/hatae/workshopmongo/services/PostService.java
@@ -0,0 +1,29 @@
+package com.daniel.hatae.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.daniel.hatae.workshopmongo.domain.Post;
+import com.daniel.hatae.workshopmongo.repository.PostRepository;
+import com.daniel.hatae.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll(){
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not Found"));
+	}
+	
+}
