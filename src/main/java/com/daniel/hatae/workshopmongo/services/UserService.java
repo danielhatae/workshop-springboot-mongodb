cfdfa0dcd4935126b8b88ed4c5d42e984e320972
danielhatae <daniel.hatae@gmail.com>
2024-10-13 22:43:07 -0300

    Inserção de usuário com POST

--- src/main/java/com/daniel/hatae/workshopmongo/services/UserService.java
+++ src/main/java/com/daniel/hatae/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.daniel.hatae.workshopmongo.domain.User;
+import com.daniel.hatae.workshopmongo.dto.UserDTO;
 import com.daniel.hatae.workshopmongo.repository.UserRepository;
 import com.daniel.hatae.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,18 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not Found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	/*
+	 *  um item um pouco polemico porque poderia ser criado no UserDTO.
+	 *  Pode haver projetos que é necessário recuperar os dados para utilizar no método de serviços.
+	*/
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 	
 }
